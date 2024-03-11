a343bed8447f2ee7c3c83738afc41625d8c70e78
Gustavo Mendes <gustavormendess@gmail.com>
2024-03-11 17:55:12 -0400

    add link pages

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -7,7 +7,16 @@ export default function Menu () {
       <li> <Link href='/'>Início</Link></li>
       <li><Link href='/produtos'>Produtos</Link></li>
       <li><Link href='/produtos/adicionar'>Adicionar Produtos</Link></li>
+      <li><Link href='/about'>About</Link> </li>
       <li><Link href='/script'>Script</Link> </li>
+      <li><Link href='/acoes'>Ações</Link> </li>
+      <li><Link href='/caches'>Caches</Link> </li>
+      <li><Link href='/home'>Home1</Link> </li>
+      <li><Link href='/home2'>Home2</Link> </li>
+      <li><Link href='/images'>Images</Link> </li>
+      <li><Link href='/imc'>IMC</Link> </li>
+      <li><Link href='/login'>Login</Link> </li>
+      
     </ul>
   )
 }
